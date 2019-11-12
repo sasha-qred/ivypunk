9a5bd1e91d8c8fb79f985c8d4d3037d7e5bb8547
sasha-qred <sashaqred@ya.ru>
2019-11-12 22:33:33 +0300

    feat: add usage of brew punk in app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/brew-punk').then((m) => m.BrewPunkRoutingModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
