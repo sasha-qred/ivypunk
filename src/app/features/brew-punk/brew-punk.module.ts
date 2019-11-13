f3b352b18c4afdbd8a6e7cf21d80fb073e77e3c8
sasha-qred <sashaqred@ya.ru>
2019-11-13 23:41:19 +0300

    feat: move brews filter to query params

--- src/app/features/brew-punk/brew-punk.module.ts
+++ src/app/features/brew-punk/brew-punk.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import {
@@ -24,6 +25,7 @@ const CONTAINERS = [BrewPunkListContainer];
     StoreModule.forFeature(brewsFeatureKey, reducer),
     EffectsModule.forFeature([BrewEffects]),
     ReactiveFormsModule,
+    RouterModule,
   ],
 })
 export class BrewPunkModule {}
--- src/app/features/brew-punk/containers/brew-punk-list/brew-punk-list.container.ts
+++ src/app/features/brew-punk/containers/brew-punk-list/brew-punk-list.container.ts
@@ -1,8 +1,16 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
+import { shareReplay, takeUntil } from 'rxjs/operators';
 import * as BrewActions from '../../actions';
 import { Brew, BrewListFilter } from '../../models';
+import { selectBrewFilter } from '../../operators';
 import { FeatureState } from '../../reducers';
 import { selectAllBrews } from '../../selectors';
 
@@ -11,22 +19,45 @@ import { selectAllBrews } from '../../selectors';
   templateUrl: './brew-punk-list.container.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrewPunkListContainer implements OnInit {
+export class BrewPunkListContainer implements OnInit, OnDestroy {
   public readonly brews$: Observable<Brew[]>;
+  public readonly brewsFilter$: Observable<Partial<BrewListFilter>>;
 
-  constructor(private store: Store<FeatureState>) {
+  private readonly destroyEvent$ = new ReplaySubject<true>(1);
+
+  constructor(
+    private store: Store<FeatureState>,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
     this.brews$ = this.store.select(selectAllBrews);
+    this.brewsFilter$ = this.route.queryParams.pipe(
+      selectBrewFilter(),
+      shareReplay({ refCount: true }),
+    );
   }
 
   public ngOnInit() {
-    this.loadBrews({});
+    this.brewsFilter$
+      .pipe(takeUntil(this.destroyEvent$))
+      .subscribe((brewsFilter) => {
+        this.loadBrews(brewsFilter);
+      });
   }
 
   public onFilterChange(brewsFilter: Partial<BrewListFilter>) {
-    this.loadBrews(brewsFilter);
+    this.router.navigate([], {
+      queryParams: brewsFilter,
+      queryParamsHandling: 'merge',
+    });
   }
 
   public loadBrews(brewsFilter: Partial<BrewListFilter>) {
     this.store.dispatch(BrewActions.loadBrews({ brewsFilter }));
+  }
+
+  public ngOnDestroy() {
+    this.destroyEvent$.next(true);
+    this.destroyEvent$.complete();
   }
 }
