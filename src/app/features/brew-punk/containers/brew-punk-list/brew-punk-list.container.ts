fb54952ad238ef04e4af66d8cee32c351368bfe8
sasha-qred <sashaqred@ya.ru>
2019-11-14 00:02:59 +0300

    feat: add initial pagination logic

--- src/app/features/brew-punk/containers/brew-punk-list/brew-punk-list.container.ts
+++ src/app/features/brew-punk/containers/brew-punk-list/brew-punk-list.container.ts
@@ -6,8 +6,8 @@ import {
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, ReplaySubject } from 'rxjs';
-import { shareReplay, takeUntil } from 'rxjs/operators';
+import { combineLatest, Observable, ReplaySubject } from 'rxjs';
+import { auditTime, map, pluck, shareReplay, takeUntil } from 'rxjs/operators';
 import * as BrewActions from '../../actions';
 import { Brew, BrewListFilter } from '../../models';
 import { selectBrewFilter } from '../../operators';
@@ -22,6 +22,9 @@ import { selectAllBrews } from '../../selectors';
 export class BrewPunkListContainer implements OnInit, OnDestroy {
   public readonly brews$: Observable<Brew[]>;
   public readonly brewsFilter$: Observable<Partial<BrewListFilter>>;
+  public readonly page$: Observable<number>;
+  public readonly prevPage$: Observable<number>;
+  public readonly nextPage$: Observable<number>;
 
   private readonly destroyEvent$ = new ReplaySubject<true>(1);
 
@@ -35,25 +38,43 @@ export class BrewPunkListContainer implements OnInit, OnDestroy {
       selectBrewFilter(),
       shareReplay({ refCount: true }),
     );
+
+    this.page$ = this.route.queryParams.pipe(
+      pluck('page'),
+      map((stringPage) => Number(stringPage)),
+      map((rawPage) => rawPage || 1),
+      shareReplay({ refCount: true }),
+    );
+    this.prevPage$ = this.page$.pipe(
+      map((page) => page - 1),
+      shareReplay({ refCount: true }),
+    );
+    this.nextPage$ = this.page$.pipe(
+      map((page) => page + 1),
+      shareReplay({ refCount: true }),
+    );
   }
 
   public ngOnInit() {
-    this.brewsFilter$
-      .pipe(takeUntil(this.destroyEvent$))
-      .subscribe((brewsFilter) => {
-        this.loadBrews(brewsFilter);
+    combineLatest(this.page$, this.brewsFilter$)
+      .pipe(auditTime(100), takeUntil(this.destroyEvent$))
+      .subscribe(([page, brewsFilter]) => {
+        this.loadBrews(page, brewsFilter);
       });
   }
 
   public onFilterChange(brewsFilter: Partial<BrewListFilter>) {
     this.router.navigate([], {
-      queryParams: brewsFilter,
+      queryParams: {
+        ...brewsFilter,
+        page: 1,
+      },
       queryParamsHandling: 'merge',
     });
   }
 
-  public loadBrews(brewsFilter: Partial<BrewListFilter>) {
-    this.store.dispatch(BrewActions.loadBrews({ brewsFilter }));
+  public loadBrews(page: number, brewsFilter: Partial<BrewListFilter>) {
+    this.store.dispatch(BrewActions.loadBrews({ page, brewsFilter }));
   }
 
   public ngOnDestroy() {
