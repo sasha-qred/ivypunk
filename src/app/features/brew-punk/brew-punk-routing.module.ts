f23561af441d0a71bb5c450d7674f37158ad71a6
sasha-qred <sashaqred@ya.ru>
2019-11-13 11:09:54 +0300

    feat: add initial route for brews list

--- src/app/features/brew-punk/brew-punk-routing.module.ts
+++ src/app/features/brew-punk/brew-punk-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrewPunkModule } from './brew-punk.module';
+import { BrewPunkListContainer } from './containers';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: BrewPunkListContainer }];
 
 @NgModule({
   imports: [BrewPunkModule, RouterModule.forChild(routes)],
